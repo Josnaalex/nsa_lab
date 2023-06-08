e0703ad02fecc410e8aa1169db92fe04e0439c68
Josna M Alexander <84304028+Josnaalex@users.noreply.github.com>
2023-06-08 14:59:36 +0530

    calcu.sh

--- calcu.sh
+++ calcu.sh
@@ -1,11 +1,15 @@
-echo "Enter two numbers"
-read a b
+i=1
+while [ $i -eq 1 ]
+do
 echo "1.Addition"
 echo "2.Substraction"
 echo "3.Multiplication"
 echo "4.Division"
+echo 
 echo "Enter option"
 read op
+echo "Enter two numbers"
+read a b
 case $op in 
 "1")
 r=$((a+b))
@@ -17,8 +21,12 @@ echo "Result "$r ;;
 r=$((a*b))
 echo "Result "$r ;;
 "4")
-r=$((a-b))
-echo "Result "$r ;;
+r="$a/$b"
+echo "scale=4; $r" | bc ;;
 *) echo "Invalid"
 esac
+echo "Do you want to continue(0/1)"
+read i
+done
+
 
